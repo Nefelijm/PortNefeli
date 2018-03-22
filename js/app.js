68560e7fe47106be0817a14a61b3b5be59cbd678
Nefelijm <sistemasnik20@gmail.com>
2018-03-22 11:02:02 -0500

    Portafolio

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
     $('.skills').cardify();
+    $(window).scroll(function() {
+        if ($("#navbar").offset().top > 56) {
+            $("#navbar").addClass("bg-inverse");
+            $('#btn-collapse').addClass("btn-collapse-color");
+           $('#span-collapse').addClass("span-collapse-color");
+        } else {
+          $("#navbar").removeClass("bg-inverse");
+          $('#btn-collapse').removeClass("btn-collapse-color");
+         $('#span-collapse').removeClass("span-collapse-color");
+        }
+      });
  
 });
